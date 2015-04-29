e37936964af8ce719c0570529f4c2061e03d32ad
Nir Elbaz <nire0510@gmail.com>
2015-04-29 12:11:21 +0300

    Update method documentation according to signature

--- www.container-site.com/scripts/editor/controllers/editor.controller.js
+++ www.container-site.com/scripts/editor/controllers/editor.controller.js
@@ -27,8 +27,7 @@
 
     /**
      * Injects selected page into container
-     * @param {string} strPageOwner The project id (requirejs) from which the page should be taken
-     * @param {string} strPageId The id of the page which should be injected
+     * @param {object} page The clicked page object which contains all necessary data for the injection
      */
     vm.selectPage = function (page) {
       var scope = $scope.$new(); // call $scope.$new() if you want to use a cloned of the parent or $scope.$new(true) if you want to create isolated scope:
@@ -36,7 +35,7 @@
 
       vm.activeDirective = page.directive;
       $document[0].title = page.title;
-      GarfieldService.inject(page.owner, page.path, page.directive, scope, '#page-editor', null, false)
+      GarfieldService.inject(page.owner, page.path, page.directive, scope, '#page-editor', false);
     }
   }
 })(angular);
